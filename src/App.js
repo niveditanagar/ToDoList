c84c0dbc19997963b70c451cf5c1807e101afb17
Nivi Nagar <nivinagar@mac.lan>
2025-12-20 16:20:24 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import ToDos from './components/ToDos';
+
+function App() {
+
+
+  return (    
+
+    <div className="App">
+      <ToDos />
+    </div>
+ 
+  );
+     
+}
+
+export default App;
---
+++ src/components/ToDos.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+
+function ToDos() {
+
+const [todos, setTodos] = useState( () => {
+const saved = localStorage.getItem("todos"); //to save it to local storage (no server needed)
+  return saved ? JSON.parse(saved) : [];
+});
+
+useEffect(() => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}, [todos]);
+
+ const [text, setText] = useState("");
+
+ //to add a new todo
+ function addTodo() { 
+   if(text.trim() === '') return;
+
+   setTodos([...todos, text]);
+   setText("");
+ }
+
+ //to remove a todo
+ function removeTodo(indextoRemove) { 
+   setTodos(todos.filter((_, index) => index !== indextoRemove)); // _ is a placeholder for the first argument which we don't use
+ }
+
+    return (
+
+        <div className="App">
+
+            <h1>My to-do App</h1>
+
+            <input 
+                value={text}
+                onChange={ (e) => setText(e.target.value)}
+                placeholder="Add a text"
+            />
+
+            <button onClick={addTodo}>Add</button>
+
+            <ul>
+                {todos.map( (todo, index) => (
+                <li key={index}>
+                {todo}
+                <button onClick={ () => removeTodo(index)}>X</button>
+                </li>
+                ))}
+            </ul>
+
+        </div>
+
+    );
+}
+
+export default ToDos;
